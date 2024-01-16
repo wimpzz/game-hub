bd24a6940b35ee620139dd4fcf870966b4fb73f2
WimpyCliff <wimpycliff@gmail.com>
2024-01-16 16:25:08 +0800

    Fetch the Games

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import theme from "./theme";
 import "./index.css";
-import NavBar from "./components/NavBar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
